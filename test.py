5f9ed343be6080e3a034c42d844be92a6d10a9a4
brice- <patrickfennelly0@utexas.edu>
2013-11-24 17:45:20 -0600

    [master] removed extraneous files

--- test.py
+++
@@ -1,8 +0,0 @@
-#Perhaps program it so it actually generates a video of the manipulated frames!
-#
-#
-import subprocess
-import shlex
-rate=2
-command2 = ("ffmpeg -qscale 2 -r %d -b 20,000 -i img%04d.png movie.mp4" %(rate))
-subprocess.call(shlex.split(command2))
